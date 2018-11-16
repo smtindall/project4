c1cccf8962d66d376e3af05d303f1afbc0df1880
Subhan Tindall <subhan.michael@gmail.com>
2018-11-16 15:45:48 -0700

    goal 1

--- parse_utils.py
+++ parse_utils.py
@@ -1,6 +1,6 @@
 import csv
 from collections import namedtuple
-
+import itertools
 from datetime import datetime
 
 def csv_parser(fname,*,delimiter=',',quotechar='"',include_header=False):
@@ -23,6 +23,13 @@ def create_named_tupple_class(fname,class_name):
     fields = extract_field_names(fname)
     return namedtuple(class_name,fields)
 
+
+def create_combo_named_tuple_class(fnames, compress_fields):
+    compress_fields = itertools.chain.from_iterable(compress_fields)
+    field_names = itertools.chain.from_iterable((extract_field_names(fname) for fname in fnames))
+    compressed_field_names = itertools.compress(field_names,compress_fields)
+    return namedtuple('Data',compressed_field_names)
+
 def iter_file(fname,class_name,parser):
     nt_class = create_named_tupple_class(fname,class_name)
     reader = csv_parser(fname)
@@ -30,3 +37,22 @@ def iter_file(fname,class_name,parser):
         parsed_data = (parse_fn(value) for value, parse_fn in zip(row,parser))
         yield nt_class(*parsed_data)
 
+
+def iter_combined_plain_tuple(fnames,class_names,parsers,compress_fields):
+    compress_fields = tuple(itertools.chain.from_iterable(compress_fields))
+    zipped_tupples = zip(*(iter_file(fname,class_name, parser)
+              for fname,class_name, parser in zip(fnames,class_names, parsers)))
+    merged_iter = (itertools.chain.from_iterable(zipped_tupple) for zipped_tupple in zipped_tupples)
+    for row in merged_iter:
+        compressed_row = itertools.compress(row,compress_fields)
+        yield tuple(compressed_row)
+
+def iter_combined(fnames,class_names,parsers,compress_fields):
+    combo_nt = create_combo_named_tuple_class(fnames, compress_fields)
+    compress_fields = tuple(itertools.chain.from_iterable(compress_fields))
+    zipped_tupples = zip(*(iter_file(fname,class_name, parser)
+              for fname,class_name, parser in zip(fnames,class_names, parsers)))
+    merged_iter = (itertools.chain.from_iterable(zipped_tupple) for zipped_tupple in zipped_tupples)
+    for row in merged_iter:
+        compressed_row = itertools.compress(row,compress_fields)
+        yield combo_nt(*compressed_row)
